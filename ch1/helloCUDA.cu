fe281cdef2d534ce9687a519cc614260b16a5eec
chenkeyi <chenkeyi_2013@163.com>
2022-08-02 22:55:51 +0800

    ch1Test

---
+++ ch1/helloCUDA.cu
@@ -0,0 +1,15 @@
+#include <stdio.h>
+__global__ void hellofromGPU(void)
+{
+	if(threadIdx.x > 5)
+		printf("hello from GPU, thread is %d\n", threadIdx.x);
+}
+
+int main()
+{
+	printf("hello CUDA, Im coming!\n");
+	hellofromGPU<<<1, 10>>>();
+	cudaDeviceReset();
+	return 0;
+}
+
